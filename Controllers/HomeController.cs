a229530d0030742f41506c08b6aec9e7e5933385
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-05 15:38:42 +0800

    Add website Login function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using DotNet6WebApp.Models;
+﻿using DotNet6WebApp.Business;
+using DotNet6WebApp.DBModels;
+using DotNet6WebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DotNet6WebApp.Controllers
@@ -7,15 +10,47 @@ namespace DotNet6WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SQL_TestContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SQL_TestContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                login.Message = "Please Confirm column is filled";
+                return View();
+            }
+
+            Users User = _context.Users.Where(x => x.Acc == login.Acc).SingleOrDefault();
+            if (User == null)
+            {
+                login.Message = "Acc Not Exist";
+                return View(login);
+            }
+
+            if (User.Pass != login.Pass)
+            {
+                login.Message = "Pass is not True";
+                return View(login);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Privacy()
--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -5,8 +5,9 @@ namespace DotNet6WebApp.ViewModels
     public class LoginViewModel
     {
         [Required]
-        public string? Acc { get; set; }
-        public string? Pass { get; set; }
+        public string Acc { get; set; }
+        [Required]
+        public string Pass { get; set; }
         public string? Message { get; set; }
     }
 }

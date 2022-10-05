3566063ddcb1026e77a9b69750165c4db65895c3
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-05 14:23:59 +0800

    Project Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet6WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

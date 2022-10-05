b35debb910e3fc8982a237a9c50b2f7bb5ac954b
DESKTOP-24QQ4C0\Administrator <bill25961073@gmail.com>
2022-10-05 14:56:36 +0800

    Add MS SQL used EFCore

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using DotNet6WebApp.Business;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SQL_TestContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -12,7 +20,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
